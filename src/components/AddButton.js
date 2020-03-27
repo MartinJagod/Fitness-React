b524ab4b616419a40a706f704ec9179e9ae417b8
MartinJagod <jagodnikmartin@gmail.com>
2020-03-27 17:59:46 -0300

    otro

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ButtomImg from '../images/add.png'
 import { Link } from 'react-router-dom'
 
-const AddButton = () => (
+const AddButton = () => ( 
 <Link to="/exercise/new">
 <img src={ButtomImg} className = "Fitness-Add" Alt="Foto"/>
 </Link>)
